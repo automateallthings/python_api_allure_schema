e01fdaa25df53cef3ec371aaa49826e936fb2c02
OlekInsp <111393481+OlekInsp@users.noreply.github.com>
2022-12-01 14:41:44 -0700

    Add log.log to gitingore. Remove temporary test class

--- tests/DELETE_empty_test.py
+++
@@ -1,9 +0,0 @@
-from utils.logger import logger
-
-log = logger('DELETE_empty_test')
-
-
-def test_one():
-    log.info("Tom " + str(200))
-    log.info("Tom " + str(201))
-    log.info("Tom " + str(202))
