b6ac608eb8d31c90378540ffab62858ed4dca961
OlekInsp <111393481+OlekInsp@users.noreply.github.com>
2022-12-08 13:42:41 -0700

    Add Allure as reporting tool. Fix log for schema_tests.py. Replace config with config_allure. Update wording for assert_status_code in response_assertions.py

--- utils/logger.py
+++ utils/logger.py
@@ -3,7 +3,7 @@ import os
 
 
 def logger(config):
-    ini_path = os.path.join(os.path.dirname(__file__), '..', 'config.ini')
+    ini_path = os.path.join(os.path.dirname(__file__), '..', 'config_allure.ini')
     ini_file = os.path.realpath(ini_path)
     logging.config.fileConfig(ini_file)
     log = logging.getLogger(config)
